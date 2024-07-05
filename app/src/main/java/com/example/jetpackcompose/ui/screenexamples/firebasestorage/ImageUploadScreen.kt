3b5c868a45741257ac44634a0c419820cd8066e7
unknown <carlos.sala.munoz@gmail.com>
2024-07-05 11:42:58 -0400

    se mejora la interfaz para para subir imagenes manualmente a firebase storage

--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/firebasestorage/ImageUploadScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/firebasestorage/ImageUploadScreen.kt
@@ -3,27 +3,35 @@ package com.example.jetpackcompose.ui.screenexamples.firebasestorage
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 
 @Composable
 fun ImageUploadScreen() {
-
     val imageUploadViewModel: ImageUploadViewModel = viewModel()
 
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
@@ -32,6 +40,8 @@ fun ImageUploadScreen() {
     ) { uri: Uri? ->
         selectedImageUri = uri
     }
+
+    val uploadProgress by imageUploadViewModel.uploadProgress.collectAsState()
 
     Column(
         modifier = Modifier
@@ -47,6 +57,10 @@ fun ImageUploadScreen() {
         Spacer(modifier = Modifier.height(16.dp))
 
         selectedImageUri?.let { uri ->
+            ImagePreview(uri = uri)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Button(onClick = {
                 imageUploadViewModel.uploadImage(
                     uri,
@@ -56,6 +70,26 @@ fun ImageUploadScreen() {
             }) {
                 Text("Upload Image")
             }
+
+            Spacer(Modifier.height(16.dp))
+
+            if (uploadProgress > 0f && uploadProgress < 100f) {
+                Text("Upload Progress: ${uploadProgress.toInt()}%")
+                LinearProgressIndicator(
+                    progress = { uploadProgress / 100 },
+                )
+            }
         }
     }
+}
+
+@Composable
+fun ImagePreview(uri: Uri) {
+    Image(
+        painter = rememberAsyncImagePainter(uri),
+        contentDescription = null,
+        modifier = Modifier
+            .size(200.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/firebasestorage/ImageUploadViewModel.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/firebasestorage/ImageUploadViewModel.kt
@@ -4,19 +4,30 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.launch
 
 class ImageUploadViewModel : ViewModel() {
-
-    private val storageReference = FirebaseStorage.getInstance().reference
+    private val storageReference: StorageReference = FirebaseStorage.getInstance().reference
+    private val _uploadProgress = MutableStateFlow(0f)
+    val uploadProgress: StateFlow<Float> = _uploadProgress
 
     fun uploadImage(uri: Uri, onSuccess: () -> Unit, onError: (Exception) -> Unit) {
         viewModelScope.launch {
             try {
                 val fileReference = storageReference.child("images/${uri.lastPathSegment}")
-                fileReference.putFile(uri).await()
-                onSuccess()
+                val uploadTask = fileReference.putFile(uri)
+                uploadTask.addOnProgressListener { taskSnapshot ->
+                    val progress = (100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount)
+                    _uploadProgress.value = progress.toFloat()
+                }.addOnSuccessListener {
+                    onSuccess()
+                }.addOnFailureListener { exception ->
+                    onError(exception)
+                }
             } catch (e: Exception) {
                 onError(e)
             }
