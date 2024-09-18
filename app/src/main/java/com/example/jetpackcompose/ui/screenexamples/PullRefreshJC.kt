cba43df38e1cdb96768a7bdd2548aea1610f76ca
unknown <carlos.sala.munoz@gmail.com>
2024-09-18 15:58:08 -0300

    se mejora visualmente pullRefresh y cada vez que se actualiza se agrega un elemento a la lista

--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/PullRefreshJC.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/PullRefreshJC.kt
@@ -1,11 +1,9 @@
 package com.example.jetpackcompose.ui.screenexamples
 
-import com.example.jetpackcompose.ui.theme.CustomComposeTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
@@ -14,7 +12,6 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,43 +21,41 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.CustomComposeTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@Preview(showSystemUi = true)
 @Composable
 fun PullRefreshJC() {
-    CustomComposeTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            val items = remember {
-                (1..100).map { "Item $it" }
-            }
-            var isRefreshing by remember {
-                mutableStateOf(false)
-            }
-            val scope = rememberCoroutineScope()
 
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-                    .padding(top = 24.dp)
-            ) {
-                PullToRefreshLazyColumn(
-                    items = items,
-                    content = {
-                        Text(text = it)
-                    },
-                    isRefreshing = isRefreshing,
-                    onRefresh = {
-                        scope.launch {
-                            isRefreshing = true
-                            delay(2000)
-                            isRefreshing = false
-                        }
+    CustomComposeTheme {
+        val items = remember {
+            mutableListOf(*(1..20).map { "Item $it" }.toTypedArray())
+        }
+
+        var isRefreshing by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            PullToRefreshLazyColumn(
+                items = items,
+                content = { Text(text = it) },
+                isRefreshing = isRefreshing,
+                onRefresh = {
+                    scope.launch {
+                        isRefreshing = true
+                        delay(1000)
+                        val newItem = "Item ${items.size + 1}"
+                        items.add(newItem)
+                        isRefreshing = false
                     }
-                )
-            }
+                }
+            )
         }
     }
 }
@@ -78,8 +73,7 @@ fun <T> PullToRefreshLazyColumn(
     val pullToRefreshState = rememberPullRefreshState(isRefreshing, onRefresh = onRefresh)
 
     Box(
-        modifier = modifier
-            .fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         LazyColumn(
             state = lazyListState,
@@ -92,6 +86,7 @@ fun <T> PullToRefreshLazyColumn(
             items(items) {
                 content(it)
             }
+
         }
         PullRefreshIndicator(
             refreshing = isRefreshing,
