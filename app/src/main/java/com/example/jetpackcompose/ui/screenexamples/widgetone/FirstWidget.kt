b0b158744558b8673c5234aa8b0035248ac71819
unknown <carlos.sala.munoz@gmail.com>
2024-10-25 18:06:48 -0300

    se corrige el codigo del widgetOne y ahora si tiene funcionalidad

--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgetone/FirstWidget.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgetone/FirstWidget.kt
@@ -6,79 +6,66 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.glance.Button
-import androidx.glance.ButtonDefaults
-import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
-import androidx.glance.GlanceTheme
+import androidx.glance.*
+import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.provideContent
 import androidx.glance.appwidget.state.updateAppWidgetState
-import androidx.glance.background
-import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
-import androidx.glance.wear.tiles.action.ActionCallback
-import androidx.glance.wear.tiles.action.actionRunCallback
-
 
 class MyAppWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-
-        // In this method, load data needed to render the AppWidget.
-        // Use `withContext` to switch to another thread for long running
-        // operations.
-
         provideContent {
-            // create AppWidget here
             MyWidget()
         }
     }
+}
 
-    @Composable
-    private fun MyWidget() {
+@Composable
+private fun MyWidget() {
 
-        GlanceTheme {
-            Scaffold(
-                titleBar = {}
+    GlanceTheme {
+        Scaffold(
+            titleBar = {}
+        ) {
+            val preferences = currentState<Preferences>()
+            val currentQuote = preferences[stringPreferencesKey("quote")] ?: getQuote()
+
+            Box(
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                /*      .clickable {
+
+                  },*/,
+                contentAlignment = Alignment.Center
             ) {
-
-                val preferences = currentState<Preferences>()
-                val currentQuote = preferences[stringPreferencesKey("quote")] ?: getQuote()
-
-                Box(
-                    modifier = GlanceModifier
-                        .fillMaxSize()
-                        .background(Color.LightGray)
-                    /*      .clickable {
-
-                      },*/,
-                    contentAlignment = Alignment.Center
+                Column(
+                    modifier = GlanceModifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Column(
-                        modifier = GlanceModifier.padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            modifier = GlanceModifier.padding(vertical = 10.dp),
-                            text = currentQuote
-                        )
+                    Text(
+                        modifier = GlanceModifier.padding(vertical = 10.dp),
+                        text = currentQuote
+                    )
 
-                        Button(
-                            text = "Change Quote",
-                            onClick = actionRunCallback(UpdateQuoteAction::class.java),
-                            modifier = GlanceModifier.padding(vertical = 10.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                contentColor = GlanceTheme.colors.onPrimary
-                            )
+                    Button(
+                        text = "Change Quote",
+                        onClick = actionRunCallback(UpdateQuoteAction::class.java),
+                        modifier = GlanceModifier.padding(vertical = 10.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = GlanceTheme.colors.onPrimary
                         )
-                    }
+                    )
                 }
             }
         }
@@ -86,7 +73,6 @@ class MyAppWidget : GlanceAppWidget() {
 }
 
 fun getQuote(): String {
-
     val quote = listOf(
         "quote 1",
         "quote 2",
@@ -96,17 +82,16 @@ fun getQuote(): String {
 }
 
 class UpdateQuoteAction : ActionCallback {
+
     override suspend fun onAction(
         context: Context,
-        glanceId: GlanceId
+        glanceId: GlanceId,
+        parameters: ActionParameters
     ) {
-
         val newQuote = getQuote()
-
         updateAppWidgetState(context, glanceId) { prefs ->
             prefs[stringPreferencesKey("quote")] = newQuote
         }
-
         MyAppWidget().update(context, glanceId)
     }
 }
