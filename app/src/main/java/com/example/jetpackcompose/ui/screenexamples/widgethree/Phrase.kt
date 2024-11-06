b1d3509ed733a09789fb14d93ba09bd706c940e2
unknown <carlos.sala.munoz@gmail.com>
2024-11-06 18:00:45 -0300

    el widget actualiza sus frases con el workmanager y ahora tambien haciendo click sobre el

--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgethree/Phrase.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgethree/Phrase.kt
@@ -1,6 +1,6 @@
 package com.example.jetpackcompose.ui.screenexamples.widgethree
 
 data class Phrase(
-    val esQuote: String,
-    val enQuote: String
+    var esQuote: String,
+    var enQuote: String
 )
--- app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgethree/QuoteWidget.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screenexamples/widgethree/QuoteWidget.kt
@@ -1,17 +1,26 @@
 package com.example.jetpackcompose.ui.screenexamples.widgethree
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.background
 import androidx.glance.color.ColorProvider
+import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
@@ -28,6 +37,9 @@ import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
 import androidx.work.*
 import java.util.concurrent.TimeUnit
+
+var myConfig = 0
+// var quote = Phrase("my phrase in spanish", "my phrase in english")
 
 class QuoteWidget : GlanceAppWidget() {
 
@@ -48,11 +60,30 @@ class QuoteWidget : GlanceAppWidget() {
 
 @Composable
 fun QuoteWidgetScreen(quote: Phrase) {
+
+    // Get a random quote for display
+    /*    if (myConfig == 0) {
+            quote = getRandomQuote()
+        } else {
+            val preferences = currentState<Preferences>()
+            quote.esQuote = (preferences[stringPreferencesKey("quote2")] ?: getRandomQuote()).toString()
+            quote.enQuote = quote.esQuote
+            myConfig = 0
+        }*/
+    if (myConfig == 1) {
+        val preferences = currentState<Preferences>()
+        quote.esQuote =
+            ((preferences[stringPreferencesKey("quoteEs")] ?: getRandomQuote().esQuote).toString())
+        quote.enQuote =
+            ((preferences[stringPreferencesKey("quoteEn")] ?: getRandomQuote().enQuote).toString())
+        myConfig = 0
+    }
     Box(
         modifier = GlanceModifier
             .fillMaxSize()
             .cornerRadius(radius = 16.dp)
-            .background(color = Color.White),
+            .background(color = Color.White)
+            .clickable(onClick = actionRunCallback(UpdateQuoteAction2::class.java)),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -98,7 +129,7 @@ fun QuoteWidgetScreen(quote: Phrase) {
 
 // Schedule the worker
 fun scheduleQuoteUpdate(context: Context) {
-    val workRequest = PeriodicWorkRequestBuilder<QuoteUpdateWorker>(15, TimeUnit.MINUTES)
+    val workRequest = PeriodicWorkRequestBuilder<QuoteUpdateWorker>(1, TimeUnit.MINUTES)
         .setConstraints(Constraints.NONE)
         .build()
 
@@ -107,4 +138,27 @@ fun scheduleQuoteUpdate(context: Context) {
         ExistingPeriodicWorkPolicy.REPLACE,
         workRequest
     )
+}
+
+class UpdateQuoteAction2 : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters
+    ) {
+        Log.d("QuoteUpdateWorker", "onClick")
+
+        myConfig = 1
+        val newQuote = getRandomQuote()
+
+        updateAppWidgetState(context, glanceId) { prefs ->
+            prefs[stringPreferencesKey("quoteEs")] = newQuote.esQuote
+            prefs[stringPreferencesKey("quoteEn")] = newQuote.enQuote
+        }
+        QuoteWidget().update(
+            context = context,
+            id = glanceId
+        )
+    }
+
 }
