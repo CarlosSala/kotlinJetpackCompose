fe1526b7aa771c77792d7e35244ccf8f0ef83aac
unknown <carlos.sala.munoz@gmail.com>
2024-11-23 23:48:37 -0300

    se migra a java 11 y se comenta un codigo deprecado relacionado a statusbar en theme.kt

--- app/src/main/java/com/example/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/theme/Theme.kt
@@ -56,14 +56,14 @@ fun CustomComposeTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
+  /*  val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
-    }
+    }*/
 
     MaterialTheme(
         colorScheme = colorScheme,
